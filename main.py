8350dd02bce602c50b228729c00deeaac5dc9fef
Sergey <sergey.abramov996.com>
2018-05-11 23:01:38 +0700

    Coding fill_L_t() and fill_R_t()

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def fill_L(rules):
+def fill_L():
     '''Build dictionary for L(U)'''
     L = {}
 
     # step 1
-    for key in rules:
+    for key in RULES:
         for elemInList in RULES[key]:
             if key in L:
                 if elemInList[:1] not in L[key]:
@@ -20,12 +20,12 @@ def fill_L(rules):
                         L[key].append(elem)
     return L
 
-def fill_R(rules):
+def fill_R():
     '''Build dictionary for R(U)'''
     R = {}
 
     # step 1
-    for key in rules:
+    for key in RULES:
         for elemInList in RULES[key]:
             if key in R:
                 if elemInList[-1:] not in R[key]:
@@ -42,11 +42,67 @@ def fill_R(rules):
                         R[key].append(elem)
     return R
 
+def fill_L_t():
+    '''Build dictionary for L_t (U)'''
+    L = fill_L()
+    L_t = {}
+
+    # step 1
+    for key in RULES:
+        for elemInList in RULES[key]:
+            if key in L_t:
+                for symbol in elemInList:
+                    if symbol in TERM:
+                        L_t[key].append(symbol)
+                        break
+            else:
+                for symbol in elemInList:
+                    if symbol in TERM:
+                        L_t[key] = list(symbol)
+                        break
+
+    # step 2
+    for key in L_t:
+        for elemInList in L[key]:
+            if elemInList in NONTERM and elemInList != key:
+                for i in L_t[elemInList]:
+                    if i not in L_t[key]:
+                        L_t[key].append(i)
+    return L_t
+
+def fill_R_t():
+    '''Build dictionary for R_t (U)'''
+    R = fill_R()
+    R_t = {}
+
+    # step 1
+    for key in RULES:
+        for elemInList in RULES[key]:
+            if key in R_t:
+                for symbol in reversed(elemInList):
+                    if symbol in TERM:
+                        R_t[key].append(symbol)
+                        break
+            else:
+                for symbol in elemInList:
+                    if symbol in TERM:
+                        R_t[key] = list(symbol)
+                        break
+
+    # step 2
+    for key in R_t:
+        for elemInList in R[key]:
+            if elemInList in NONTERM and elemInList != key:
+                for i in R_t[elemInList]:
+                    if i not in R_t[key]:
+                        R_t[key].append(i)
+    return R_t
+
 def main():
-    L = fill_L(RULES)
-    R = fill_R(RULES)
-    print (L)
-    print (R)
+    L_t = fill_L_t()
+    R_t = fill_R_t()
+    print (L_t)
+    print (R_t)
 
 if __name__ == '__main__':
 
@@ -54,7 +110,7 @@ if __name__ == '__main__':
 
     RULES = {'A' : ['!B!'], 'B' : ['B+T', 'T'], 'T' : ['T*M', 'M'], 'M' : ['I', '(B)']}
     # NUMBER_ALT = {'B': 2, 'T': 2, 'M': 2}
-    TERM = {'!', '(', ')', '+', '*', 'a', 'b'}
+    TERM = {'!', '(', ')', '+', '*', 'I'}
     NONTERM = {'A', 'B', 'T', 'M'}
     First_PLACE_in_RULES = {'A' : 1, 'B': 2, 'T': 4, 'M': 6}
 
